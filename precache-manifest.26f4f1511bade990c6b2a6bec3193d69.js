86e4a6d792160d31a813a7040a88ee776e454b51
k1ns3 <k1ns3za@gmail.com>
2020-07-02 13:37:25 +0300

    SHM-frontend

---
+++ precache-manifest.26f4f1511bade990c6b2a6bec3193d69.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7179e6c726c6581669b6763ae649c9ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6129b610dc641c22220",
+    "url": "/static/css/main.cfd3f6b8.chunk.css"
+  },
+  {
+    "revision": "b06f00d7775bb59d693b",
+    "url": "/static/js/2.e48607e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e48607e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6129b610dc641c22220",
+    "url": "/static/js/main.35bc838f.chunk.js"
+  },
+  {
+    "revision": "7a64fd46d1d706601ce8",
+    "url": "/static/js/runtime-main.dba7775a.js"
+  },
+  {
+    "revision": "18f7bac3af79d726cf8dfaaf889fc31f",
+    "url": "/static/media/F-32.wfp.18f7bac3.jpg"
+  },
+  {
+    "revision": "5aa5e6201c606883d4921680ed271334",
+    "url": "/static/media/bg_header.5aa5e620.png"
+  },
+  {
+    "revision": "6d03891b86da443ff089703963587ae0",
+    "url": "/static/media/icons8-удалить.6d03891b.svg"
+  },
+  {
+    "revision": "0f9fcbf0305b11be21b93a9630e9b944",
+    "url": "/static/media/spez1_090316.0f9fcbf0.png"
+  },
+  {
+    "revision": "6be90e603dfa5cbfe33d41c19071ac9d",
+    "url": "/static/media/шипы-квадрат-для-сайта.6be90e60.jpg"
+  }
+]);
